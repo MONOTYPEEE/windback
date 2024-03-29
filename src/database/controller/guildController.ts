945119d6d5c188a52d9c777321f6ea8031264a60
김상구 <spair0039@gmail.com>
2024-03-29 10:53:12 +0900

    add findGuild method

--- src/database/controller/guildController.ts
+++ src/database/controller/guildController.ts
@@ -1,13 +1,27 @@
 import Guild from "../schema/Guild"
 
 async function updateGuild(guildid:string, icon:string|null, name:string){
-    await Guild.findByIdAndUpdate(guildid, {
-        "_id": guildid,
-        "icon": icon,
-        "name": name
-    },{
-        upsert: true
-    })
+    try{
+        await Guild.findByIdAndUpdate(guildid, {
+            "_id": guildid,
+            "icon": icon,
+            "name": name
+        },{
+            upsert: true
+        })
+    }
+    catch(error){
+        console.log(error)
+    }
 }
 
-export default { updateGuild }
+async function findGuild(guildid:string){
+    try{
+        return await Guild.findById(guildid)
+    }
+    catch(error){
+        console.log(error)
+    }
+}
+
+export default { updateGuild, findGuild }
