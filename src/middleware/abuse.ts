9a4398603c65fba78202f1f597b71fb07519ae59
김상구 <spair0039@gmail.com>
2024-04-01 11:59:13 +0900

    add AbuseMatch middleware

---
+++ src/middleware/abuse.ts
@@ -0,0 +1,19 @@
+import { Message } from "discord.js";
+import { abuseEnglish, abuseKorean } from "../utils/dictionary";
+import userController from "../database/controller/userController";
+
+export default function AbueseMatch(message:Message){
+    const matchedKorean = message.content.match(abuseKorean),
+        matchedEnglish = message.content.match(abuseEnglish)
+    
+    if(matchedKorean){
+        matchedKorean.forEach(word=>{
+            userController.updateKeyword(word, message.author.id, message.guildId ?? '')
+        })
+    }
+    if(matchedEnglish){
+        matchedEnglish.forEach(word=>{
+            userController.updateKeyword(word, message.author.id, message.guildId ?? '')
+        })
+    }
+}
