67d1193f765fcac0a404f3116530e25dd1d32c74
김상구 <spair0039@gmail.com>
2024-04-03 11:07:56 +0900

    add updateMention method

--- src/database/controller/userController.ts
+++ src/database/controller/userController.ts
@@ -1,9 +1,9 @@
 import User from "../schema/User";
 
+const currentYear = new Date().getFullYear()
+
 async function updateKeyword(keyword:string, userid:string, guildid:string, amount:number = 1){
     try{
-        const currentYear = new Date().getFullYear()
-
         await User.findByIdAndUpdate({
             'guildId': guildid,
             'userId': userid,
@@ -28,7 +28,20 @@ async function updateEmoji(emoji:string, userid:string, guildid:string, amount:n
 }
 
 async function updateMention(mentionedUserId:string, userid:string, guildid:string, amount:number = 1){
-
+    try{
+        await User.findByIdAndUpdate({
+            'guildId': guildid,
+            'userId': userid,
+            'year': currentYear
+        },{
+            $inc: { [`statistic.mention.${mentionedUserId}`]: amount }
+        },{
+            upsert: true
+        })
+    }
+    catch(error){
+        console.log(error)
+    }
 }
 
 export default { updateKeyword, updateActivity, updateEmoji, updateMention }
