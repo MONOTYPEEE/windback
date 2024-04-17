d34648f771c45c6ac8fb9a2f0f711f52ba213d3b
김상구 <spair0039@gmail.com>
2024-04-17 13:20:53 +0900

    update updateActivity controller

--- src/database/controller/userController.ts
+++ src/database/controller/userController.ts
@@ -1,6 +1,8 @@
 import User from "../schema/User";
 
-const currentYear = new Date().getFullYear()
+const currentYear = new Date().getFullYear(),
+    currentDayofWeek = new Date().getDay(),
+    currentHour = new Date().getHours()
 
 async function updateKeyword(keyword:string, userid:string, guildid:string, amount:number = 1){
     try{
@@ -20,11 +22,25 @@ async function updateKeyword(keyword:string, userid:string, guildid:string, amou
 }
 
 async function updateActivity(userid:string, guildid:string, amount:number = 1){
-
+    try{
+        await User.findByIdAndUpdate({
+            'guildId': guildid,
+            'userId': userid,
+            'year': currentYear
+        },{
+            $inc: {
+                [`statistic.activity.daily.${currentHour}`]: amount,
+                [`statistic.activity.weekly.${currentDayofWeek}`]: amount
+            }
+        })
+    }
+    catch(error){
+        console.log(error)
+    }
 }
 
 async function updateEmoji(emoji:string, userid:string, guildid:string, amount:number = 1){
-
+    
 }
 
 async function updateMention(mentionedUserId:string, userid:string, guildid:string, amount:number = 1){
