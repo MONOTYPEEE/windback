ed3689ab436cee1837d352f9854ffcb8dfb2b956
김상구 <spair0039@gmail.com>
2024-05-22 16:03:16 +0900

    more pre-processing actions

--- bot/utils/Preprocess.ts
+++ bot/utils/Preprocess.ts
@@ -1,8 +1,10 @@
-import { URLregex } from "./dictionary";
+import { customEmojiRegex, unicodeEmojiRegex, URLregex } from "./dictionary";
 
 export default function Preprocess(string:string){
     string = string.replace(URLregex, '') //remove URL
         .replace(/<@[0-9]{17,19}>/giu, '') //remove mention
+        .replace(customEmojiRegex, '')
+        .replace(unicodeEmojiRegex, '')
 
     return string
 }
