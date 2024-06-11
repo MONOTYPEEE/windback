16b293be27c84de1af9f458d8fa1173565748030
김상구 <spair0039@gmail.com>
2024-06-11 09:04:14 +0900

    refresh, revoke token

--- server/controller/auth.ts
+++ server/controller/auth.ts
@@ -1,11 +1,8 @@
-import { config } from "../../database/config"
-import { Request, Response } from "express"
+import { config } from '../../database/config'
+import { Request, Response } from 'express'
 import axios from 'redaxios'
 
 export async function getToken(request:Request, response:Response){
-
-    console.log(request?.body)
-
     if(!request.body){
         return response.status(400).send('code is required')
     }
@@ -17,7 +14,7 @@ export async function getToken(request:Request, response:Response){
                 'client_id': config.APPID,
                 'client_secret': config.SECRET,
                 'code': request.body.code,
-                'grant_type': "authorization_code",
+                'grant_type': 'authorization_code',
                 'redirect_uri': 'http://localhost:5173'
             }),
             {
@@ -28,8 +25,6 @@ export async function getToken(request:Request, response:Response){
         )
         .then((d)=>{
             if(d.status === 200){
-                console.log(d)
-
                 const expiresAt = new Date(Date.now() + d.data.expires_in)
 
                 return response.status(200).send({
@@ -41,5 +36,70 @@ export async function getToken(request:Request, response:Response){
         })
         .catch((error)=>{
             return response.status(400).send('error while get token')
+        })
+}
+
+export async function refreshAccessToken(request:Request, response:Response){
+    if(!request.body){
+        return response.status(400).send('token is required')
+    }
+
+    axios
+        .post(
+            `${config.APP_BASEURL}/oauth2/token`,
+            new URLSearchParams({
+                'client_id': config.APPID,
+                'client_secret': config.SECRET,
+                'grant_type': 'refresh_token',
+                'refresh_token': request.body.token
+            }),
+            {
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                },
+                auth: `Barer `
+            }
+        )
+        .then((d)=>{
+            if(d.status === 200){
+                const expiresAt = new Date(Date.now() + d.data.expires_in)
+
+                return response.status(200).send({
+                    access: d.data.access_token,
+                    refresh: d.data.refresh_token,
+                    expires: expiresAt
+                })
+            }
+        })
+        .catch((error)=>{
+            return response.status(400).send('error while refresh token')
+        })
+}
+
+export async function revokeToken(request:Request, response:Response){
+    if(!request.headers.authorization){
+        return response.status(401).send('You need a Token so you can revoke it')
+    }
+
+    axios
+        .post(
+            `${config.APP_BASEURL}/oauth2/token/revoke`,
+            new URLSearchParams({
+                'token': request.headers.authorization
+            }),
+            {
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                    'Authorization': `Basic ${btoa(config.APPID + ':' + config.SECRET)}`
+                },
+            }
+        )
+        .then((d)=>{
+            if(d.status === 200){
+                return response.status(200).send('Access Revoked')
+            }
+        })
+        .catch((error)=>{
+            return response.status(400).send('error while revoke token')
         })
 }
--- server/router/auth.ts
+++ server/router/auth.ts
@@ -1,8 +1,10 @@
 import express, { Router } from "express";
-import { getToken } from "../controller/auth";
+import { getToken, refreshAccessToken, revokeToken } from "../controller/auth";
 
 const authRouter:Router = express.Router()
 
 authRouter.post('/login', getToken)
+authRouter.post('/refresh', refreshAccessToken)
+authRouter.post('/revoke', revokeToken)
 
 export default authRouter
