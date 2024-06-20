173f3642c327042a5420ef68366124f7c704f1ac
김상구 <spair0039@gmail.com>
2024-06-20 13:59:24 +0900

    getGuildInfo 2

--- server/router/guild.ts
+++ server/router/guild.ts
@@ -1,8 +1,9 @@
 import express, { Router } from 'express'
-import { getAvaliableGuild } from '../controller/guild'
+import { getAvaliableGuild, getGuildInfo } from '../controller/guild'
 
 const guildRouter:Router = express.Router()
 
 guildRouter.get('/avaliable', getAvaliableGuild)
+guildRouter.get('/:id(\\d{13,16})', getGuildInfo)
 
 export default guildRouter
