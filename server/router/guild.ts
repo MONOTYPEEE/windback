dd1cb20b7d98228734d26dfe1e0f0fb06fa1e0f0
김상구 <spair0039@gmail.com>
2024-06-20 19:53:57 +0900

    Revert "getGuildInfo 2"
    This reverts commit 173f3642c327042a5420ef68366124f7c704f1ac.

--- server/router/guild.ts
+++ server/router/guild.ts
@@ -1,9 +1,8 @@
 import express, { Router } from 'express'
-import { getAvaliableGuild, getGuildInfo } from '../controller/guild'
+import { getAvaliableGuild } from '../controller/guild'
 
 const guildRouter:Router = express.Router()
 
 guildRouter.get('/avaliable', getAvaliableGuild)
-guildRouter.get('/:id(\\d{13,16})', getGuildInfo)
 
 export default guildRouter
